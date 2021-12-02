587ecc2d1c2246afcbe012c7dbd876898805eceb
NagisaCo <yjr888@vip.qq.com>
2021-12-02 19:15:47 +0800

    feat(photos): 添加外键约束

--- nano_backend/apps/photos/models.py
+++ nano_backend/apps/photos/models.py
@@ -12,10 +12,14 @@ class Photo(models.Model):
     image = models.ImageField(upload_to='photos/%Y/%m/%d/')
     type = models.IntegerField(choices=ImageTypeChoice.choices, default=ImageTypeChoice.REAL, verbose_name='照片类型')
 
+    anime_id = models.ForeignKey('animes.Anime', on_delete=models.CASCADE, verbose_name='动画')
+    place_id = models.ForeignKey('places.Place', on_delete=models.CASCADE, verbose_name='地点')
+
     create_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='photo_create_user',
                                     verbose_name='创建者')
 
-    status = models.IntegerField(choices=StatusChoice.choices, default=StatusChoice.UNPUBLISHED, verbose_name='状态')
+    is_public = models.BooleanField(default=False, verbose_name='是否公开')
+    is_approved = models.BooleanField(default=False, verbose_name='是否通过审核')
 
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
--- nano_backend/apps/photos/serializers.py
+++ nano_backend/apps/photos/serializers.py
@@ -9,24 +9,9 @@ class PhotoSerializer(ModelSerializer):
     class Meta:
         model = Photo
         fields = '__all__'
-        read_only_fields = ['create_user', 'status']
+        read_only_fields = ['create_user', 'is_approved']
 
     def validate(self, attrs):
         # attrs['create_user'] = self.context['request'].user
-        # attrs['contributor'] = self.context['request'].user
         attrs['create_user'] = users.models.User.objects.get(pk=1)
-        attrs['status'] = StatusChoice.PENDING
         return attrs
-
-
-class PhotoInfoList(ListSerializer):
-    def to_representation(self, data):
-        data = data.filter(status=StatusChoice.PASS)
-        return super(PhotoInfoList, self).to_representation(data)
-
-
-class PhotoInfoSerializer(ModelSerializer):
-    class Meta:
-        model = Photo
-        list_serializer_class = PhotoInfoList
-        fields = ['id', 'name', 'image']
