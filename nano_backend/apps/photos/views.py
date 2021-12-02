c0812eaaf581850e015222cb99730eb2b43d40b2
NagisaCo <yjr888@vip.qq.com>
2021-12-02 20:49:52 +0800

    feat(animes, places): 自动更新贡献者

--- nano_backend/apps/photos/views.py
+++ nano_backend/apps/photos/views.py
@@ -3,6 +3,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
+from animes.models import Anime
+from places.models import Place
 from users.models import User
 from .models import Photo
 from .serializers import PhotoDetailSerializer
@@ -13,7 +15,8 @@ class PhotoViewSet(ModelViewSet):
     filter_backends = [OrderingFilter, DjangoFilterBackend]  # 排序 过滤
     serializer_class = PhotoDetailSerializer
 
-    ordering_fields = ['id', 'create_time', 'update_time', 'is_approved', 'is_public', 'anime_id', 'place_id', 'create_user']  # 排序
+    ordering_fields = ['id', 'create_time', 'update_time', 'is_approved', 'is_public', 'anime_id', 'place_id',
+                       'create_user']  # 排序
     filter_fields = ['id', 'is_approved', 'is_public', 'anime_id', 'place_id', 'create_user']  # 过滤
 
     def update(self, request, *args, **kwargs):
@@ -22,3 +25,35 @@ class PhotoViewSet(ModelViewSet):
         """
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
+
+    def update_contributor(self):
+        """
+        更新贡献者
+        :return:
+        """
+        anime = None
+        place = None
+        if self.action == 'create':  # 创建
+            if self.request.data.get('is_public', False) is True:  # 已公开
+                # 从请求中获取id
+                anime = Anime.objects.get(pk=self.request.data['anime_id'])
+                place = Place.objects.get(pk=self.request.data['place_id'])
+        elif self.action == 'update':  # 更新
+            if self.request.data.get('is_public', False) is True:  # 已公开
+                # 从数据库中查询
+                photo = Photo.objects.get(pk=self.kwargs['pk'])
+                anime = photo.anime_id
+                place = photo.place_id
+        if anime and place:  # 更新贡献者
+            anime.contributor.add(User.objects.get(pk=2))
+            place.contributor.add(User.objects.get(pk=2))
+            anime.save()
+            place.save()
+
+    def perform_create(self, serializer):
+        self.update_contributor()
+        serializer.save()
+
+    def perform_update(self, serializer):
+        self.update_contributor()
+        serializer.save()
--- nano_backend/apps/places/views.py
+++ nano_backend/apps/places/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
+from animes.models import Anime
 from users.models import User
 from .filters import PlaceFilter
 from .models import Place
@@ -27,3 +28,29 @@ class PlaceViewSet(ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(Q(is_approved=True) | Q(create_user=User.objects.get(pk=1)))
 
+    def update_contributor(self):
+        """
+        更新贡献者
+        :return:
+        """
+        anime = None
+        if self.action == 'create':  # 创建
+            if self.request.data.get('is_public', False) is True:  # 已公开
+                # 从请求中获取id
+                anime = Anime.objects.get(pk=self.request.data['anime_id'])
+        elif self.action == 'update':  # 更新
+            if self.request.data.get('is_public', False) is True:  # 已公开
+                # 从数据库中查询
+                photo = Place.objects.get(pk=self.kwargs['pk'])
+                anime = photo.anime_id
+        if anime:  # 更新贡献者
+            anime.contributor.add(User.objects.get(pk=2))
+            anime.save()
+
+    def perform_create(self, serializer):
+        self.update_contributor()
+        serializer.save()
+
+    def perform_update(self, serializer):
+        self.update_contributor()
+        serializer.save()
