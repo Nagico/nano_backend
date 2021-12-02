caaacc356ec29dc075457a39f939060471538843
NagisaCo <yjr888@vip.qq.com>
2021-12-02 19:31:49 +0800

    feat(photos): 支持put部分更新

--- nano_backend/apps/photos/views.py
+++ nano_backend/apps/photos/views.py
@@ -7,4 +7,10 @@ from .serializers import PhotoSerializer
 class PhotoViewSet(ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
-    
+
+    def update(self, request, *args, **kwargs):
+        """
+        支持部分更新
+        """
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
