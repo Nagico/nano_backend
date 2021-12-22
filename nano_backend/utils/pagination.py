ebfe125a5c30b7f9196d3c31c6e9e8a7cc5cb319
NagisaCo <yjr888@vip.qq.com>
2021-12-22 12:26:19 +0800

    fix: 修复分页配置

--- nano_backend/utils/pagination.py
+++ nano_backend/utils/pagination.py
@@ -8,4 +8,5 @@ class GlobalPageNumberPagination(PageNumberPagination):
     def __init__(self):
         super(GlobalPageNumberPagination, self).__init__()
         self.page_size_query_param = 'page_size'
-        self.max_page_size = 20
+        self.page_size = 20
+        self.max_page_size = 200
