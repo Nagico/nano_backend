6aad1af001d11723d230b66cd0b3b2b5201c235e
NagisaCo <yjr888@vip.qq.com>
2021-12-24 22:48:53 +0800

    fix(admin): 修复字段错误

--- nano_backend/apps/photos/admin.py
+++ nano_backend/apps/photos/admin.py
@@ -13,4 +13,4 @@ class PhotoAdmin(StaffRequiredAdminMixin, admin.ModelAdmin):
     list_filter = ['type', 'is_public', 'is_approved', 'anime_id', 'place_id', 'create_user']
     search_fields = ['id', 'name', 'type', 'is_public', 'is_approved', 'anime_id__title',
                      'anime_id__title_cn', 'place_id__name', 'create_user__username']
-    autocomplete_fields = ['anime', 'place']  # 带有搜索框的外键选择框
+    autocomplete_fields = ['anime_id', 'place_id']  # 带有搜索框的外键选择框
--- nano_backend/apps/places/admin.py
+++ nano_backend/apps/places/admin.py
@@ -12,4 +12,4 @@ class PlaceAdmin(StaffRequiredAdminMixin, admin.ModelAdmin):
     list_display_links = ['id', 'name']
     list_filter = ['is_public', 'city', 'is_approved', 'create_user', 'update_time', 'create_time']
     search_fields = ['id', 'name', 'city', 'address', 'anime_id__title_cn', 'anime_id__title', 'create_user__username']
-    autocomplete_fields = ['anime']  # 带有搜索框的外键选择框
+    autocomplete_fields = ['anime_id']  # 带有搜索框的外键选择框
